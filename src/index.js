b57f70d75b563f6584501b63d22163677283777b
Mark Pokrovsky <solexofficial@yandex.ru>
2021-12-27 23:34:58 +0300

    feat: init for redux prototype

--- src/index.js
+++ src/index.js
@@ -1,23 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { compose, pipe } from 'lodash/fp';
 
 const App = () => {
-  const x = 2;
-
-  const double = number => number * 2;
-  const square = number => number * number;
-  const half = number => number / 2;
-
-  const divide = num2 => {
-    return function (num1) {
-      return num1 / num2;
-    };
-  };
-
-  const mathCalculate = pipe(double, square, half, divide(3));
-
-  return <h1>{mathCalculate(x)}</h1>;
+  return <h1>APP</h1>;
 };
 
 ReactDOM.render(
