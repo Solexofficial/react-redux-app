f51409bbbac9803986e766f1d2b2c20b76103dc2
Mark Pokrovsky <solexofficial@yandex.ru>
2021-12-27 23:48:12 +0300

    feat: add create store & dispatch

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function createStore(initialState) {
+  let state = initialState;
+
+  function getState() {
+    return state;
+  }
+
+  function dispatch(action) {
+    console.log(action);
+    if (action.type === 'task/completed') {
+      const newArray = [...state];
+      const elementIndex = newArray.findIndex(el => el.id === action.payload.id);
+      newArray[elementIndex].completed = true;
+      state = newArray;
+      console.log(state);
+    }
+  }
+  return { getState, dispatch };
+}
+
+const store = createStore([{ id: 1, description: 'Task 1', completed: false }]);
+
 const App = () => {
-  return <h1>APP</h1>;
+  console.log(store.getState());
+
+  return (
+    <>
+      <h1>APP</h1>
+      <button onClick={() => store.dispatch({ type: 'task/completed', payload: { id: 1 } })}>Complete</button>
+    </>
+  );
 };
 
 ReactDOM.render(
