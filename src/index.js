4c61b53eaeaa1b750107bb2d5200edb51f8ee048
Mark Pokrovsky <solexofficial@yandex.ru>
2021-12-27 23:12:20 +0300

    feat: add curry

--- src/index.js
+++ src/index.js
@@ -9,7 +9,13 @@ const App = () => {
   const square = number => number * number;
   const half = number => number / 2;
 
-  const mathCalculate = pipe(double, square, half);
+  const divide = num2 => {
+    return function (num1) {
+      return num1 / num2;
+    };
+  };
+
+  const mathCalculate = pipe(double, square, half, divide(3));
 
   return <h1>{mathCalculate(x)}</h1>;
 };
