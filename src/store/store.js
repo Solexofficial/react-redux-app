32b8a1479829419e6c1a0fdabf26f6af178022fa
Mark Pokrovsky <solexofficial@yandex.ru>
2021-12-28 23:21:26 +0300

    feat: add redux toolkit create reducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,10 @@
 import { createStore } from 'redux';
 import taskReducer from './task';
 
-const initialState = [
-  { id: 1, title: 'Task 1', completed: false },
-  { id: 2, title: 'Task 2', completed: false },
-];
+
 
 function configureStore() {
-  return createStore(taskReducer, initialState);
+  return createStore(taskReducer);
 }
 
 export default configureStore;
--- src/store/task.js
+++ src/store/task.js
@@ -1,4 +1,9 @@
-import { createAction } from '@reduxjs/toolkit';
+import { createAction, createReducer } from '@reduxjs/toolkit';
+
+const initialState = [
+  { id: 1, title: 'Task 1', completed: false },
+  { id: 2, title: 'Task 2', completed: false },
+];
 
 const update = createAction('task/updated');
 const remove = createAction('task/removed');
@@ -18,21 +23,14 @@ export function taskDeleted(id) {
   return remove({ id });
 }
 
-function taskReducer(state = [], action) {
-  switch (action.type) {
-    case update.type: {
-      const newArray = [...state];
-      const elementIndex = newArray.findIndex(el => el.id === action.payload.id);
-
-      newArray[elementIndex] = { ...newArray[elementIndex], ...action.payload };
-      return newArray;
-    }
-    case remove.type: {
-      return state.filter(task => task.id !== action.payload.id);
-    }
-    default:
-      return state;
-  }
-}
+const taskReducer = createReducer(initialState, builder => {
+  builder.addCase(update, (state, action) => {
+    const elementIndex = state.findIndex(el => el.id === action.payload.id);
+    state[elementIndex] = { ...state[elementIndex], ...action.payload };
+  });
+  builder.addCase(remove, (state, action) => {
+    return state.filter(task => task.id !== action.payload.id);
+  });
+});
 
 export default taskReducer;
