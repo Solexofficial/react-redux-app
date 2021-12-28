aceda4a9c10553e8264fe5276e2304d78f47b23d
Mark Pokrovsky <solexofficial@yandex.ru>
2021-12-28 23:08:42 +0300

    feat: add redux toolkit, create action

--- src/store/task.js
+++ src/store/task.js
@@ -1,37 +1,33 @@
-const TASK_UPDATED = 'task/updated';
-const TASK_DELETED = 'task/deleted';
+import { createAction } from '@reduxjs/toolkit';
+
+const update = createAction('task/updated');
+const remove = createAction('task/removed');
+
+// const TASK_UPDATED = 'task/updated';
+// const TASK_DELETED = 'task/deleted';
 
 export function taskCompleted(id) {
-  return {
-    type: TASK_UPDATED,
-    payload: { id, completed: true },
-  };
+  return update({ id, completed: true });
 }
 
 export function titleChanged(id) {
-  return {
-    type: TASK_UPDATED,
-    payload: { id, title: `New Title for ${id}` },
-  };
+  return update({ id, title: `New Title for ${id}` });
 }
 
 export function taskDeleted(id) {
-  return {
-    type: TASK_DELETED,
-    payload: { id },
-  };
+  return remove({ id });
 }
 
 function taskReducer(state = [], action) {
   switch (action.type) {
-    case TASK_UPDATED: {
+    case update.type: {
       const newArray = [...state];
       const elementIndex = newArray.findIndex(el => el.id === action.payload.id);
 
       newArray[elementIndex] = { ...newArray[elementIndex], ...action.payload };
       return newArray;
     }
-    case TASK_DELETED: {
+    case remove.type: {
       return state.filter(task => task.id !== action.payload.id);
     }
     default:
